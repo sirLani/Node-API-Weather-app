5b1304605060fb9be2d2beccdb6042d1f39fbac4
Olaide <laide.tech@rebasive.com>
2021-07-17 23:31:57 +0100

    feat: implement search

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-﻿
+﻿const weatherForm = document.querySelector("form");
+const searchitem = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
+  const location = searchitem.value;
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
